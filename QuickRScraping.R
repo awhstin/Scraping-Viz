15814740f9701f61195b29d56382e689130450f1
James Austin Wehrwein <jawehrwein@gmail.com>
2016-03-23 13:30:41 -0500

    Create QuickRScraping.R
    This is a quick loop that takes a dataframe of two columns, column 1 is the url for the news source. Column 2 is the xml path for the sections you want scraped. Included is the Bing Sentiment value for each article. The resulting dataframe includes the article text, news source line, and sentiment value.

---
+++ QuickRScraping.R
@@ -0,0 +1,18 @@
+library(syuzhet)
+library(rvest)
+library(plyr)
+
+news<- read.csv("H:/news.csv", stringsAsFactors=FALSE)
+newslist<-NULL
+for(i in 1:nrow(news)){
+article<-read_html(news[i,1])%>%
+    xml_nodes(news[i,2]) %>%
+    html_text() 
+article<-gsub("[^[:alnum:]]", " ", article)
+alist<-as.list(article)
+dat<- do.call(rbind.fill.matrix,alist)
+sentiment<-(get_sentiment(as.vector(na.omit(dat)), method = "bing"))
+newssource<-data.frame(article,news[i,1],sentiment,stringsAsFactors = FALSE)
+rename(newssource, c('news.i..1.'='Source'))
+newslist<-rbind(newslist,newssource)
+}
