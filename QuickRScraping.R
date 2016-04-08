603e2c13b777e03ef1ccc7582746e3fed7b68db3
James Austin Wehrwein <jawehrwein@gmail.com>
2016-04-08 13:46:15 -0500

    Update QuickRScraping.R
    Removed sentiment factor, it was causing loop issues. The sentiment analysis can be done afterwards

--- QuickRScraping.R
+++ QuickRScraping.R
@@ -11,10 +11,7 @@ article<-read_html(news[i,1])%>%
     xml_nodes(news[i,2]) %>%
     html_text() 
 article<-gsub("[^[:alnum:]]", " ", article)
-alist<-as.list(article)
-dat<- do.call(rbind.fill.matrix,alist)
-sentiment<-(get_sentiment(as.vector(na.omit(dat)), method = "bing"))
-newssource<-data.frame(article,news[i,1],sentiment,stringsAsFactors = FALSE)
+newssource<-data.frame(article,news[i,1],stringsAsFactors = FALSE)
 rename(newssource, c('news.i..1.'='Source'))
 newslist<-rbind(newslist,newssource)
 }
